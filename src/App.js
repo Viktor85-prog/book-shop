6fff7d890ac7161108facda28488cee87f75ffcb
viktor <viktor.bulkin@stremline-c.com>
2022-03-09 14:46:05 +0300

    add semantic-ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { setBooks } from './actions/books'
 import axios from 'axios'
+import { Menu } from 'semantic-ui-react'
+
 // import books from './books.json'
 
 class App extends Component {
@@ -14,20 +16,55 @@ class App extends Component {
       })
   }
   render() {
-    const { books } = this.props
+    const { books, isReady } = this.props
     return (
-      <ul>
-        {!books ? 'Loading...' : books.map(book => (<li key={books.id}>
-          <b>{book.title}</b> - {book.author}
-        </li>))}
-      </ul>
+      <div>
+        <Menu>
+          <Menu.Item
+            name='browse'
+            onClick={this.handleItemClick}
+          >
+            Browse
+          </Menu.Item>
+
+          <Menu.Item
+            name='submit'
+            onClick={this.handleItemClick}
+          >
+            Submit
+          </Menu.Item>
+
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='signup'
+              onClick={this.handleItemClick}
+            >
+              Sign Up
+            </Menu.Item>
+
+            <Menu.Item
+              name='help'
+              onClick={this.handleItemClick}
+            >
+              Help
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <ul>
+          {!isReady ? 'Loading...' : books.map(book => (<li key={books.id}>
+            <b>{book.title}</b> - {book.author}
+          </li>))}
+        </ul>
+      </div>
     )
 
   }
 }
 
 const mapStateToProps = ({ books }) => ({
-  books: books.items
+  books: books.items,
+  isReady: books.isReady
+
 })
 
 const mapDispatchToProps = dispatch => ({
