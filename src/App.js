2d672bb0b82be6063d64480623adc1631416ec0d
viktor <viktor.bulkin@stremline-c.com>
2022-03-09 15:26:51 +0300

    del semanti, add navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { setBooks } from './actions/books'
 import axios from 'axios'
-import { Menu } from 'semantic-ui-react'
+import HeadMenu from './components/Menu'
 
 // import books from './books.json'
 
@@ -19,37 +19,7 @@ class App extends Component {
     const { books, isReady } = this.props
     return (
       <div>
-        <Menu>
-          <Menu.Item
-            name='browse'
-            onClick={this.handleItemClick}
-          >
-            Browse
-          </Menu.Item>
-
-          <Menu.Item
-            name='submit'
-            onClick={this.handleItemClick}
-          >
-            Submit
-          </Menu.Item>
-
-          <Menu.Menu position='right'>
-            <Menu.Item
-              name='signup'
-              onClick={this.handleItemClick}
-            >
-              Sign Up
-            </Menu.Item>
-
-            <Menu.Item
-              name='help'
-              onClick={this.handleItemClick}
-            >
-              Help
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
+        <HeadMenu />
         <ul>
           {!isReady ? 'Loading...' : books.map(book => (<li key={books.id}>
             <b>{book.title}</b> - {book.author}
---
+++ src/components/Menu.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './menu.css'
+
+
+// function updatemenu() {
+//     if (document.getElementById('responsive-menu').checked == true) {
+//         document.getElementById('menu').style.borderBottomRightRadius = '0';
+//         document.getElementById('menu').style.borderBottomLeftRadius = '0';
+//     } else {
+//         document.getElementById('menu').style.borderRadius = '0px';
+//     }
+// }
+
+const HeadMenu = () => (
+    <nav id='menu'>
+        <input type='checkbox' id='responsive-menu' onclick='updatemenu()' /><label></label>
+        <ul>
+            <li><a href='http://'>Магазин</a></li>
+            <li><a class='dropdown-arrow' href='http://'>Products</a>
+                <ul class='sub-menus'>
+                    <li><a href='http://'> Sub Menu 1</a></li>
+                    <li><a href='http://'> Sub Menu 2</a></li>
+                    <li><a href='http://'> Sub Menu 3</a></li>
+                    <li><a href='http://'> Sub Menu 4</a></li>
+                </ul>
+            </li>
+            <li><a href='http://'>Итого: &nbsp;<b>0</b> руб.</a></li>
+            <li><a href='http://'>Корзина <b>0</b></a></li>
+        </ul>
+    </nav>
+)
+
+export default HeadMenu
