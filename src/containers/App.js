a6f5ceac32a59f7f218a2fab3603795586ecd32c
viktor <viktor.bulkin@stremline-c.com>
2022-03-15 17:08:19 +0300

    add lodash sort

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,29 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as booksActions from '../actions/books'
 import App from '../components/app/App'
+import orderBy from 'lodash/orderBy'
+
+const sortBy = (books, filterBy) => {
+    switch (filterBy) {
+        case 'all':
+            return books
+        case 'price_high':
+            return orderBy(books, 'price', 'desc')
+        case 'price_low':
+            return orderBy(books, 'price', 'asc')
+        case 'popular':
+            return orderBy(books, 'rating', 'desc')
+        case 'author':
+            return orderBy(books, 'author', 'asc')
+        default: return books
+    }
+
+}
 
 
 
 const mapStateToProps = ({ books }) => ({
-    books: books.items,
+    books: sortBy(books.items, books.filterBy),
     isReady: books.isReady
 
 })
